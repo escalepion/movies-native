607c5d3c632d0ac93b55aa3914434acc0d95fc02
escalepion <escalepion@gmail.com>
2017-07-23 16:31:01 +0300

    add redux form to login page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+
+
 import MoviesReducer from './movies_reducer';
 
 export default combineReducers({
+form,
 movies: MoviesReducer
 });
--- src/screens/auth/LogIn.js
+++ src/screens/auth/LogIn.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
+import { reduxForm, Field } from 'redux-form';
 import { Content, Form, Item, Input, Button, Text } from 'native-base';
 
+const renderField = ({ input, placeholder, keyboardType, secureTextEntry, meta: { touched, error } }) => (
+  <View>
+  <Item placeholderLabel>
+    <Input 
+    style={{ flex: 1 }} 
+    {...input} 
+    keyboardType={keyboardType} 
+    placeholder={placeholder}
+    secureTextEntry={secureTextEntry}
+    />
+  </Item>
+  {touched && error && <Text style={styles.errorText}>{error}</Text>}
+  </View>
+);
+ 
 class LogIn extends Component {
     handleOnPress() {
         this.props.navigation.navigate('SignUp');
     }
     render() {
+      const { handleSubmit } = this.props;
         return (
         <Content>
           <Form>
-            <Item placeholderLabel>
-              <Input style={{ flex: 1 }} placeholder="Username" />
-            </Item>
-            <Item last placeholderLabel>
-              <Input style={{ flex: 1 }} placeholder="Password" />
-            </Item>
+            <Field 
+            name='email'
+            placeholder="E-mail"
+            keyboardType='email-address'
+            component={renderField}
+            />
+            <Field
+            secureTextEntry 
+            name='password'
+            placeholder="Password"
+            component={renderField}
+            />
           </Form>
-          <Button block style={{ margin: 15, marginTop: 50 }}>
+          <Button onPress={handleSubmit(this.handleOnPress.bind(this))} block style={{ margin: 15, marginTop: 50 }}>
             <Text>Sign In</Text>
           </Button>
         </Content>
@@ -24,4 +48,30 @@ class LogIn extends Component {
     }
 }
 
-export default LogIn;
+function validate(values) {
+    const errors = {};
+
+    if (!values.email) {
+        errors.email = 'Pls enter a mail adress';
+    }
+    if (!values.password) {
+        errors.password = 'Pls enter a password';
+    }
+
+    return errors;
+}
+
+const styles = {
+errorText: {
+    fontSize: 10,
+    margin: 5,
+    color: 'red'
+}
+};
+
+const LogInForm = reduxForm({
+    form: 'login',
+    validate
+})(LogIn);
+
+export default LogInForm;
