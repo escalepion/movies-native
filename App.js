8262a951be44dc948b2139527c9fba2874bb6931
escalepion <escalepion@gmail.com>
2017-07-23 17:44:29 +0300

    add firebase

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Expo from 'expo';
+import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { Container, Text } from 'native-base'; 
+import { Container, Spinner } from 'native-base'; 
 
 import reducers from './src/reducers';
 import { RootNavigator } from './src/navigation/routes';
@@ -12,12 +13,22 @@ class App extends React.Component {
    state = {
     isReady: false,
    }
-    async componentWillMount() {
+async componentWillMount() {
   await Expo.Font.loadAsync({
     'Roboto': require('native-base/Fonts/Roboto.ttf'),
     'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
   });
   this.setState({ isReady: true });
+
+const config = {
+    apiKey: 'AIzaSyDJcXKuZI13TUWzKK6kXdZA3Gxu1XuvMNk',
+    authDomain: 'movies-a4783.firebaseapp.com',
+    databaseURL: 'https://movies-a4783.firebaseio.com',
+    projectId: 'movies-a4783',
+    storageBucket: 'movies-a4783.appspot.com',
+    messagingSenderId: '790483019248'
+  };
+  firebase.initializeApp(config);
   }
   render() {
     const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -32,7 +43,7 @@ class App extends React.Component {
      }
     return (
       <Container>
-        <Text>Font loading.</Text>
+        <Spinner />
       </Container>
     );
   }
