963bca3d2f90f00dd45d4f1030322afddc42a0b0
escalepion <escalepion@gmail.com>
2017-07-25 20:52:36 +0300

    fixed

--- src/components/common/Comments.js
+++ src/components/common/Comments.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { View, ListView } from 'react-native';
 import { connect } from 'react-redux';
+import { List, ListItem, Left, Thumbnail, Body, Text, Right } from 'native-base';
 
 import * as actions from '../../actions/comments';
 import CommentContainer from './CommentContainer';
@@ -36,6 +37,22 @@ return <CommentContainer key={comment.uid} comment={comment} />;
             dataSource={this.dataSource}
             renderRow={this.renderRow}
             />
+        <List
+            dataArray={this.props.comments} renderRow={data =>
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail source={{ uri: 'https://s3.amazonaws.com/37assets/svn/1065-IMG_2529.jpg' }} />
+                </Left>
+                <Body>
+                  <Text>{data.comment}</Text>
+                  <Text numberOfLines={3} note>{data.comment}</Text>
+                </Body>
+                <Right>
+                  <Text note>3.43</Text>
+                </Right>
+              </ListItem>
+        }
+          />
         </View>
         );
     }
